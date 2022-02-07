0c8c954f18d831c855beebf9e448f342a3a0278f
Rajan Abishai Madhukar <themailboxofabi@yahoo.com>
2022-02-07 15:57:48 +0530

    Infinite background work
    partially done

--- Assets/Scripts/BgSpawner.cs
+++ Assets/Scripts/BgSpawner.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class BgSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject[] bgs;
+    private float height;
+    private float highest_Y_Pos;
+
+    private void Awake()
     {
-        
+        bgs = GameObject.FindGameObjectsWithTag(TagManager.BACKGROUND_TAG); //gets all the objects tagged with this and puts them in the array
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        height = bgs[0].GetComponent<BoxCollider2D>().size.y; //using the element at index 0 to determine height
+
+        highest_Y_Pos = bgs[0].transform.position.y;
+    }
+
+
     void Update()
     {
         
